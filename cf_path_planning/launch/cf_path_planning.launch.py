77fbda65460b5551ba06960923ea8a300c0c2676
Oszi <oszimilian@oszimilian.net>
2024-12-28 00:06:00 +0100

    add basic structure for path planing and some rviz stuff

---
+++ cf_path_planning/launch/cf_path_planning.launch.py
@@ -0,0 +1,31 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+from launch.actions import IncludeLaunchDescription, DeclareLaunchArgument
+from launch.substitutions import LaunchConfiguration
+
+def generate_launch_description():
+
+    input_grid_path_arg = DeclareLaunchArgument(
+        'input_grid_path',
+        default_value='default.json',
+        description='path to the json-file which describs the grid of the arena'
+    )
+
+    input_grid_path = LaunchConfiguration('input_grid_path')
+
+    path_planing = Node(
+        package='cf_path_planning',
+        executable='path_planning',
+        output='screen',
+        parameters=[{
+            'input_grid_path': input_grid_path
+        }]
+    )
+
+
+    return LaunchDescription(
+        [
+            input_grid_path_arg,
+            path_planing
+        ]
+    )
